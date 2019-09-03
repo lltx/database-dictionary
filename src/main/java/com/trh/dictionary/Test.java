5ec24b823fe149fe1057e15f5f099491a60d4096
tangxushine@163.com <tangxushine@163.com>
2019-09-03 11:30:11 +0800

    Merge branch 'master' of E:\idea\database-dictionary with conflicts.

--- src/main/java/com/trh/dictionary/Test.java
+++ src/main/java/com/trh/dictionary/Test.java
@@ -2,7 +2,7 @@ package com.trh.dictionary;
 
 import com.trh.dictionary.bean.TableInfo;
 import com.trh.dictionary.service.BuildPDF;
-import com.trh.dictionary.service.oracleservice.OracleDatabase;
+import com.trh.dictionary.service.db2.Db2Executor;
 import com.trh.dictionary.service.sqlserver.BuildSqlserverPDF;
 import org.apache.commons.io.FileUtils;
 
@@ -15,30 +15,28 @@ import java.util.List;
  */
 public class Test {
     @org.junit.Test
-    public  void  testMakePdf(){
-        String  FILE_DIR = "F:/pdf/";
-        BuildPDF.MakePdf("localhost", "cd_core", "3306", "root", "root",FILE_DIR,"cd_core");
+    public void testMakePdf() {
+        String FILE_DIR = "F:/pdf/";
+        BuildPDF.MakePdf("localhost", "cd_core", "3306", "root", "root", FILE_DIR, "cd_core");
     }
 
     @org.junit.Test
-    public  void  testMakeSqlServerPdf(){
-        String  FILE_DIR = "F:/pdf/";
-        BuildSqlserverPDF.MakePdf("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA",FILE_DIR,"zhou");
+    public void testMakeSqlServerPdf() {
+        String FILE_DIR = "F:/pdf/";
+        BuildSqlserverPDF.MakePdf("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA", FILE_DIR, "zhou");
     }
+
     @org.junit.Test
-    public  void  testMakeOraclePdf(){
-        try {
-            List<TableInfo> tableInfo = OracleDatabase.getTableInfo("jdbc:oracle:thin:@//127.0.0.1:1521/orcl","root","123456");
-            if (tableInfo.size() == 0) {
-                return;
-            }
-            String filePath = "F:/pdf/";
-            FileUtils.forceMkdir(new File(filePath));
-            //带目录
-            BuildPDF.build(filePath, tableInfo, "Oraclecd_core8");
-            System.out.println("生成数据字典完毕,一共生成了"+tableInfo.size()+"条数据");
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void testMakeDb2ServerPdf() throws Exception {
+        List<TableInfo> tableInfo = Db2Executor.getDB2Tables("192.168.171.230", "TEST", "db2", "system");
+        if (tableInfo.size() == 0) {
+            return;
         }
+        String filePath = "E:/pdf/";
+        FileUtils.forceMkdir(new File(filePath));
+        //带目录
+        BuildPDF.build(filePath, tableInfo, "Db2");
     }
+
+
 }
---
+++ src/main/java/com/trh/dictionary/service/db2/Db2Executor.java
@@ -0,0 +1,209 @@
+package com.trh.dictionary.service.db2;
+
+import com.mysql.jdbc.StringUtils;
+import com.trh.dictionary.bean.ColumnInfo;
+import com.trh.dictionary.bean.IndexInfo;
+import com.trh.dictionary.bean.TableInfo;
+import com.trh.dictionary.util.SqlExecutor;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author TangXu
+ * @create 2019-09-02 17:32
+ * @description:
+ */
+public class Db2Executor {
+
+    private static String HOST = "192.168.171.230";
+    private static String SCHEMA = "TEST";
+    private static String USER = "db2";
+    private static String PASSWORD = "system";
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Connection connection = SqlExecutor.newDB2Connection(HOST, SCHEMA, USER, PASSWORD);
+        List<TableInfo> db2Tables = getDB2Tables(connection, SCHEMA);
+        for (TableInfo table : db2Tables) {
+            getDb2Columns(connection, table.getTableName(), SCHEMA);
+            getTableIndexMap(connection, table.getTableName());
+        }
+    }
+
+    /**
+     * 查询表结构
+     *
+     * @param connection
+     * @param table
+     * @param schema
+     * @throws SQLException
+     */
+    public static List<ColumnInfo> getDb2Columns(Connection connection, String table, String schema) throws SQLException {
+        String sql = "SELECT " +
+                " T.COLNO, " +
+                " T.COLNAME, " +
+                " T.REMARKS, " +
+                " T.TYPENAME, " +
+                " T.LENGTH, " +
+                " T.SCALE, " +
+                " T.DEFAULT, " +
+                " T.NULLS " +
+                "FROM " +
+                " SYSCAT.COLUMNS T " +
+                "WHERE " +
+                " T.TABSCHEMA =  '" + schema + "'" +
+                " AND T.TABNAME =  '" + table + "'" +
+                " ORDER BY T.COLNO";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        List<ColumnInfo> columns = new ArrayList<ColumnInfo>();
+        while (rs.next()) {
+            ColumnInfo columnInfo = new ColumnInfo();
+            columnInfo.setOrder(rs.getInt("COLNO") + 1);
+            columnInfo.setName(rs.getString("COLNAME").trim());
+            columnInfo.setDescription(rs.getString("REMARKS").trim());
+            columnInfo.setIsNull(rs.getString("NULLS").trim());
+            columnInfo.setDefaultValue(StringUtils.isNullOrEmpty(rs.getString("DEFAULT")) ? "" : rs.getString("DEFAULT").trim());
+            columnInfo.setType(rs.getString("TYPENAME").trim() + "(" + rs.getString("LENGTH").trim() + ")");
+            columnInfo.setIsIndex(isIndex(connection, table, rs.getString("COLNAME").trim()));
+            columns.add(columnInfo);
+        }
+        return columns;
+    }
+
+    /**
+     * 判断是否是索引
+     *
+     * @param connection
+     * @param table
+     * @param columnName
+     * @return
+     * @throws SQLException
+     */
+    public static int isIndex(Connection connection, String table, String columnName) throws SQLException {
+        List<Map<String, Object>> tableIndexes = getTableIndexMap(connection, table);
+        for (Map map : tableIndexes) {
+            if (map.containsKey(columnName)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 获取表信息
+     *
+     * @param connection
+     * @param table
+     * @return
+     * @throws SQLException
+     */
+    public static List<Map<String, Object>> getTableIndexMap(Connection connection, String table) throws SQLException {
+        List<Map<String, Object>> indexList = new ArrayList<Map<String, Object>>();
+        String sql = "SELECT INDNAME, COLNAMES, UNIQUERULE FROM SYSCAT.INDEXES WHERE TABNAME = '" + table + "'";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            Map<String, Object> indexMap = new HashMap<String, Object>();
+            String indexName = rs.getString("INDNAME");
+            String columnName = rs.getString("COLNAMES");
+            columnName = columnName.substring(1);
+//             String uniqueRule = rs.getString("UNIQUERULE");
+            indexMap.put(columnName, indexName);
+            indexList.add(indexMap);
+        }
+        return indexList;
+    }
+
+    /**
+     * 获取表信息
+     *
+     * @param connection
+     * @param table
+     * @return
+     * @throws SQLException
+     */
+    public static List<IndexInfo> getTableIndexList(Connection connection, String table) throws SQLException {
+        List<IndexInfo> indexList = new ArrayList<IndexInfo>();
+        String sql = "SELECT IID, INDNAME, COLNAMES, UNIQUERULE FROM SYSCAT.INDEXES WHERE TABNAME = '" + table + "'";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            IndexInfo indexInfo = new IndexInfo();
+            String indexName = rs.getString("INDNAME");
+            String columnName = rs.getString("COLNAMES");
+            columnName = columnName.substring(1);
+            String uniqueRule = rs.getString("UNIQUERULE");
+            indexInfo.setType(uniqueRule);
+            indexInfo.setName(indexName);
+            indexInfo.setContainKey(columnName);
+            int tablePrimaryKey = getTablePrimaryKey(connection, table, columnName);
+            indexInfo.setIsIndex(tablePrimaryKey);
+            indexInfo.setOrder(rs.getInt("IID"));
+            indexList.add(indexInfo);
+        }
+        return indexList;
+    }
+
+    /**
+     * 判斷是否是主鍵
+     *
+     * @param connection
+     * @param table
+     * @param columnName
+     * @return
+     * @throws SQLException
+     */
+    public static int getTablePrimaryKey(Connection connection, String table, String columnName) throws SQLException {
+        String sql = "SELECT CONSTNAME, COLNAME FROM SYSCAT.KEYCOLUSE WHERE TABNAME = '" + table + "'";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            String primaryKey = rs.getString("CONSTNAME");
+            String column_name = rs.getString("COLNAME");
+            if (column_name.equalsIgnoreCase(columnName)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static List<TableInfo> getDB2Tables(String host, String schema, String user, String password) throws SQLException, ClassNotFoundException {
+        Connection connection = SqlExecutor.newDB2Connection(host, schema, user, password);
+        List<TableInfo> db2Tables = getDB2Tables(connection, SCHEMA);
+        return db2Tables;
+    }
+
+    /**
+     * 获取表
+     *
+     * @param connection
+     * @param schema
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    public static List<TableInfo> getDB2Tables(Connection connection, String schema) throws SQLException, ClassNotFoundException {
+        List<TableInfo> tables = new ArrayList<TableInfo>();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet rs = metaData.getTables(null, schema, "%", new String[]{"TABLE"});
+        while (rs.next()) {
+            String tableName = rs.getString("TABLE_NAME");
+            String remarks = rs.getString("REMARKS");
+            if (StringUtils.isNullOrEmpty(tableName)) {
+                continue;
+            }
+            TableInfo tableInfo = new TableInfo();
+            tableInfo.setTableName(tableName);
+            tableInfo.setDescription(remarks);
+            tableInfo.setColumnList(getDb2Columns(connection, tableName, schema));
+            tableInfo.setIndexInfoList(getTableIndexList(connection, tableName));
+            tables.add(tableInfo);
+        }
+        return tables;
+    }
+}
