260c3c0b96fc0a41785b74ec8c7723f715abf8eb
tangxushine@163.com <tangxushine@163.com>
2019-09-09 15:47:05 +0800

    get db2 user schemas

--- src/test/java/com/trh/dictionary/Test.java
+++ src/test/java/com/trh/dictionary/Test.java
@@ -7,11 +7,13 @@ import com.trh.dictionary.service.oracleservice.OracleDatabase;
 import com.trh.dictionary.service.postgreSQL.BuildPgSqlPdf;
 import com.trh.dictionary.service.sqlserver.BuildSqlserverPDF;
 import com.trh.dictionary.service.sqlserver.WriteSqlserverMarkDown;
+import com.trh.dictionary.util.SqlExecutor;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.sql.Connection;
 import java.util.List;
 
 /**
@@ -23,20 +25,20 @@ public class Test {
     static Logger logger = LoggerFactory.getLogger(Test.class);
 
     @org.junit.Test
-    public  void  testMakeMySqlPdf(){
-        String  FILE_DIR = "F:/pdf/";
+    public void testMakeMySqlPdf() {
+        String FILE_DIR = "F:/pdf/";
         //生成PDF文件
-        BuildPDF.MakePdf("localhost", "cd_core", "3306", "root", "root",FILE_DIR,"cd_core");
+        BuildPDF.MakePdf("localhost", "cd_core", "3306", "root", "root", FILE_DIR, "cd_core");
         //生成MARKDOWN文件
-        BuildPDF.MakeMarkDown("localhost", "cd_core", "3306", "root", "root",FILE_DIR);
+        BuildPDF.MakeMarkDown("localhost", "cd_core", "3306", "root", "root", FILE_DIR);
     }
 
     @org.junit.Test
-    public  void  testMakePgSqlPdf(){
-        String  FILE_DIR = "F:/pdf/";
+    public void testMakePgSqlPdf() {
+        String FILE_DIR = "F:/pdf/";
         //生成PDF文件
         try {
-            BuildPgSqlPdf.buildPdf("192.168.161.3", "mydata", "54321", "postgres", "root",FILE_DIR,"mydata");
+            BuildPgSqlPdf.buildPdf("192.168.161.3", "mydata", "54321", "postgres", "root", FILE_DIR, "mydata");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,17 +47,16 @@ public class Test {
     }
 
 
-
     @org.junit.Test
-    public  void  testMakeSqlServerPdf(){
-        String  FILE_DIR = "F:/pdf/";
-        BuildSqlserverPDF.MakePdf("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA",FILE_DIR,"zhou");
+    public void testMakeSqlServerPdf() {
+        String FILE_DIR = "F:/pdf/";
+        BuildSqlserverPDF.MakePdf("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA", FILE_DIR, "zhou");
     }
 
     @org.junit.Test
-    public  void  testMakeOraclePdf(){
+    public void testMakeOraclePdf() {
         try {
-            List<TableInfo> tableInfo = OracleDatabase.getTableInfo("jdbc:oracle:thin:@//127.0.0.1:1521/orcl","root","123456");
+            List<TableInfo> tableInfo = OracleDatabase.getTableInfo("jdbc:oracle:thin:@//127.0.0.1:1521/orcl", "root", "123456");
             if (tableInfo.size() == 0) {
                 return;
             }
@@ -65,14 +66,16 @@ public class Test {
             BuildPDF.build(filePath, tableInfo, "Oraclecd_core12");
             String markdown = BuildPDF.writeMarkdown(tableInfo);
             System.out.println(markdown);
-            System.out.println("生成数据字典完毕,一共生成了"+tableInfo.size()+"条数据");
+            System.out.println("生成数据字典完毕,一共生成了" + tableInfo.size() + "条数据");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @org.junit.Test
     public void testMakeDb2ServerPdf() throws Exception {
-        List<TableInfo> tableInfo = Db2Executor.getDB2Tables("192.168.171.230", 50000, "TEST", "db2", "system");
+        Connection connection = SqlExecutor.newDB2Connection("192.168.171.230", 50000, "TEST", "db2", "system");
+        List<TableInfo> tableInfo = Db2Executor.getDB2Tables(connection, "TEST");
         if (tableInfo.size() == 0) {
             return;
         }
@@ -85,13 +88,13 @@ public class Test {
 
 
     @org.junit.Test
-    public  void  testMakeSqlServerMarkdown(){
-        String  FILE_DIR = "F:/pdf/";
-        WriteSqlserverMarkDown.MakeMarkdown("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA",FILE_DIR);
+    public void testMakeSqlServerMarkdown() {
+        String FILE_DIR = "F:/pdf/";
+        WriteSqlserverMarkDown.MakeMarkdown("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA", FILE_DIR);
     }
 
     @org.junit.Test
-    public  void  testMakeSqlServerMarkdownStrig(){
+    public void testMakeSqlServerMarkdownStrig() {
         logger.info(WriteSqlserverMarkDown.MakeMarkdownString("192.168.161.3", "zhou", "1433", "SA", "zhoufan123AAA"));
     }
 
